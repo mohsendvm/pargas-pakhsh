1abb1c5e357a1e94624c2ea16ab29a3da7b8057c
mohsendvm <mohsenrazmi4352@gmail.com>
2025-09-18 19:19:17 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.post('/orders', (req, res) => {
     console.log('سفارش دریافت شد:', order);
     res.json({status: 'ok'});
 });
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
